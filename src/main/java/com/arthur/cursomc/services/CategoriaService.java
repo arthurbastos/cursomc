86a44676e36b75bb0cf30c674e8cfc8279af3fdb
arthurbastos <arthurcoelho.27@gmail.com>
2020-08-04 16:05:15 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/arthur/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.arthur.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthur.cursomc.domain.Categoria;
+import com.arthur.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
