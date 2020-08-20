735938798084bc14c3690b2a0cb4c03b5f88366a
arthurbastos <arthurcoelho.27@gmail.com>
2020-08-20 17:37:24 -0300

    Enviando imagem via endpoint

--- src/main/java/com/arthur/cursomc/resources/ClienteResource.java
+++ src/main/java/com/arthur/cursomc/resources/ClienteResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arthur.cursomc.domain.Cliente;
@@ -82,6 +84,12 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok(listDto);
+	}
+	
+	@RequestMapping(value="/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestParam(name = "file") MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
